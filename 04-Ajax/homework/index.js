a7a1f44c8e82df7f869e9834a882062b473b4799
TomassReynoso <tomasreynoso17@gmail.com>
2022-09-01 16:40:14 -0300

    Homework 04

---
+++ 04-Ajax/homework/index.js
@@ -0,0 +1,42 @@
+var url = 'http://localhost:5000/amigos';
+
+$('#loading').hide();
+
+
+$('#boton').click(() => {
+
+
+    $.get(`${url}`, (info) => {
+        const lista = $('#lista');
+        lista.empty();
+        for (let amigo of info) {
+            const li = document.createElement('li');
+            li.className = "amigos";
+            li.id = amigo.id;
+            li.innerText = amigo.name;
+            lista.append(li);
+        }
+    })
+    // cursor: pointer;
+});
+
+$(search).click(()=>{
+   
+    $.get(`${url}` + '/' + $('#input').val(),(amigo) => {
+        $('#amigo').text(amigo.name);
+    });
+
+});
+
+$('#delete').click(() => {
+
+    $.get(`${url}` + '/' + $('#inputDelete').val(),(amigo) => {
+        $.ajax({
+            url:`${url}` + '/' + $('#inputDelete').val(),
+            type:'DELETE',
+            success: (result) => {
+                $('#success').text(`${amigo.name} se ha eliminado de la lista`);
+            }
+        });
+    });
+});
