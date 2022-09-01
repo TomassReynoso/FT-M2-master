d90910303b9bd579fb68f8a6ade5e3da163a61db
TomassReynoso <tomasreynoso17@gmail.com>
2022-09-01 14:36:59 -0300

    Homework 03

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,4 +1,4 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
+var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
   if (typeof startEl === "undefined") {
@@ -9,16 +9,34 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+
+  if (matchFunc(startEl)) resultSet.push(startEl);
+  for (let i = 0; i < startEl.children.length; i++) {
+    let result = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+    resultSet = [...resultSet, ...result];
+  }
+  return resultSet;
+
+  // if (matchFunc(startEl)) resultSet.push(startEl);
+  // for (var el of startEl.children) {
+  //   resultSet = resultSet.concat(traverseDomAndCollectElements(matchFunc, el))         // ALTERNATIVA
+  // }
+  // return resultSet;
+
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 
-var selectorTypeMatcher = function(selector) {
+var selectorTypeMatcher = function (selector) {
   // tu código aquí
-  
+
+  if (selector[0] === "#") return "id";
+  if (selector[0] === ".") return "class";
+  if (selector.includes(".")) return "tag.class";
+  return "tag";
+
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -26,22 +44,45 @@ var selectorTypeMatcher = function(selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
+var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
+  if (selectorType === "id") {
+    matchFunction = (element) => "#" + element.id === selector;
+    // matchFunction = (el) => `#${el.id}` === selector;  // ALTERNATIVA
   } else if (selectorType === "class") {
-    
+    matchFunction = (element) => {
+      for (const cls of element.classList) {
+        if ("." + cls === selector) return true;
+      }
+      return false;
+    };
+    // matchFunction = (el) => el.classList.contains(selector.substring(1));   // ALTERNATIVA
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (element) => {
+      const [miTag, miClass] = selector.split(".");
+      return (
+        matchFunctionMaker(miTag)(element) &&
+        matchFunctionMaker("." + miClass)(element)
+      );
+    };
+    // matchFunction = (el) => {
+    //   const [tag, className] = selector.split(`.`);
+    //   return (el.classList.contains(className) &&                  // ALTERNATIVA
+    //     el.tagName.toLowerCase() === tag.toLowerCase()
+    //   );
+    // };
   } else if (selectorType === "tag") {
-    
+    matchFunction = (element) => {
+      if (element.tagName.toLowerCase() === selector) return true;
+      return false;
+    };
+    // matchFunction = (el) => el.tagName.toLowerCase() === selector.toLowerCase();       // ALTERNATIVA
   }
   return matchFunction;
 };
 
-var $ = function(selector) {
+var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
