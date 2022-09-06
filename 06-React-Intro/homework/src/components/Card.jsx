ddd22d6df6754543e5ceec026ac11b9c7d301078
TomassReynoso <tomasreynoso17@gmail.com>
2022-09-06 10:44:14 -0300

    Homework 06

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose }) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <button onClick={onClose}>X</button>
+      <div>
+        <span>{name}</span>
+      </div>
+      <div>
+        <div>
+          <span>Min</span>
+          <span> {min}</span>
+        </div>
+        <div>
+          <span>Máx</span>
+          <span> {max}</span>
+        </div>
+        <div>
+          <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+// return (
+//   <div>
+//    <p>Máx: {max}</p>
+//    <p>Min: {min}</p>
+//    <p>Name: {name}</p>
+//    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="" />
+//    <button onClick={onClose}>X</button>
+//   </div>
+// );
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-export default function SearchBar(props) {
+export default function SearchBar(onSearch) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <input type="text" name="search" id="search" placeholder="Busca tu ciudad..."/>
+      <button onClick={() => onSearch("Algo")}>Agregar</button>
+    </div>
+  );
 };
